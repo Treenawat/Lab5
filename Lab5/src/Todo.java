7cd05a0545fd7e621371701994d68cef2dda8d65
Yo Manomairat <treenawat.manomairat-1@ou.edu>
2019-02-14 12:41:08 -0600

    Upload

---
+++ Lab5/src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-01-24
+OU ID_______________: 113470031
+OU EMAIL____________: Treenawat.Manomairat-1@ou.edu
+
+Tasks:
+1. Finish animal and equipment
+2. Finish animaltest, equipmenttest and zootest
+
+Estimated Time (minutes):
+1. 40
+2. 60
+
+Actual Time (minutes):
+1. 30
+2. 200
+*/}
